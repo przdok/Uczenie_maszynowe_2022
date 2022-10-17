cddc4f20714cf57ca0e864d1fe8b5033e648d089
Przemyslaw Doktor <prz.dok@gmail.com>
2022-10-17 14:02:40 +0200

    feat: pierwsze zadanie

---
+++ zad_1.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    
+    def is_passed(self):
+        if self.marks > 50:
+            return True
+        else:
+            return False
+
+s1 = Student('Maria', 51)
+s2 = Student('Andrzej', 49)
+print(f'Czy {s1.name} zdała? {s1.is_passed()}')
+print(f'Czy {s2.name} zdała? {s2.is_passed()}')
