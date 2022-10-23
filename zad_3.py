db6b50b03447a581a79c656979374de23696c373
Przemyslaw Doktor <prz.dok@gmail.com>
2022-10-23 15:13:46 +0200

    feat: zad_3 finalna wersja

---
+++ zad_3.py
@@ -0,0 +1,30 @@
+class Property:
+    def __init__(self, area, rooms, price, address):
+        self.area = area
+        self.rooms = rooms
+        self.price = price
+        self.address = address
+
+    def __str__(self):
+        return f'Area: {self.area}\nNumber of rooms: {self.rooms}\nPrice: {self.price}\nAddress: {self.address}\n'
+
+class House(Property):
+    def __init__(self, area, rooms, price, address, plot):
+        super().__init__(area, rooms, price, address)
+        self.plot = plot
+    
+    def __str__(self):
+        return f'{super().__str__()}Plot: {self.plot}\n\n'
+
+class Flat(Property):
+    def __init__(self, area, rooms, price, address, floor):
+        super().__init__(area, rooms, price, address)
+        self.floor = floor
+
+    def __str__(self):
+        return f'{super().__str__()}Floor number: {self.floor}\n\n'
+
+H = House('24m2', 3, 100000, 'Downing Street 4, 43-000 Katowice', 250)
+F = Flat('24m2', 3, 100000, 'Downing Street 4, 43-000 Katowice', 4)
+
+print(H.__str__()+F.__str__())
