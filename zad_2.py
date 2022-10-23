8077fadf0db098304c4b6bbf8ebb50f2e0cc13f9
Przemyslaw Doktor <prz.dok@gmail.com>
2022-10-23 14:50:04 +0200

    feat: zad_2 finalna wersja

--- zad_2.py
+++ zad_2.py
@@ -1,13 +1,9 @@
-from ssl import OP_NO_TLSv1_2
-import string
-
-
 class Student:
     def __init__(self, name):
         self._name = name
 
     def _str_(self):
-        return f'Name: {self._name}'
+        return f'Student:\n{self._name}\n'
 
 
 class Library:
@@ -19,7 +15,7 @@ class Library:
         self._phone = phone
 
     def _str_(self):
-        return f'Biblioteka:\n{self._street}, {self._zip_code} {self._city}\n{self._phone}\n\n'
+        return f'Biblioteka:\n{self._street}, {self._zip_code} {self._city}\nTel.: {self._phone}\n'
 
 class Employee:
     def __init__(self, first_name, last_name, hire_date, birth_date, city, street, zip_code, phone):
@@ -33,7 +29,7 @@ class Employee:
         self._phone = phone
 
     def _str_(self):
-        return f'Pracownik:\n{self._first_name} {self._last_name}\n{self._phone}\n\n'
+        return f'Pracownik:\n{self._first_name} {self._last_name}, {self._phone}\n\n'
 
 class Book:
     def __init__(self, library, publication_date, author_name, author_surname, number_of_pages):
@@ -45,17 +41,17 @@ class Book:
 
 
     def _str_(self):
-        return f'Dane książki:\n{self._library._str_()}\n{self._publication_date}\n{self._author_name} {self._author_surname}\n\n'
+        return f'{self._library._str_()}\n{self._author_name} {self._author_surname}\n'
 
 class Order:
     def __init__(self, employee, student, books, order_date):
         self._employee = employee
         self._student = student
-        self._books = string.Join(' ', books)
+        self._books = '\n'.join([x._str_() for x in books])
         self._order_date = order_date
 
     def _str_(self):
-        return f'{self._employee._str_()} {self._student._str_()} {self._books._str_()} {self._order_date._str_()}'
+        return f'***\n{self._employee._str_()}{self._student._str_()}\n{self._books}\nData zamówienia:\n{self._order_date}\n***\n\n'
 
 s1 = Student("Adam Kowalski")
 s2 = Student("Bartosz Kowalski")
@@ -80,4 +76,4 @@ books2 = [b4, b5]
 o1 = Order(e1, s2, books1, "2022-10-17")
 o2 = Order(e2, s1, books2, "2022-10-16")
 
-print(o1._str_(), o1._str_())
+print(o1._str_() + o1._str_())
